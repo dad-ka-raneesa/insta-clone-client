36772c805a64edb51b7ae17357db1017cd068e01
Deepika <bhuvanadeepika017@gmail.com>
2020-09-19 12:15:54 +0530

    added profile component and css for it

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <h1>Profile</h1>
+    <div className='profile'>
+      <div className='profile-container'>
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ2DY2WktwMebkhOn7kGhkwXoxHa4EzrU9Nag&usqp=CAU"
+          />
+        </div>
+        <div>
+          <h4>Isabella</h4>
+          <div className='info-tab'>
+            <h6>20 Posts</h6>
+            <h6>10 Followers</h6>
+            <h6>5 Following</h6>
+          </div>
+        </div>
+      </div>
+      <div className='gallery'>
+        <img className='item' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ2DY2WktwMebkhOn7kGhkwXoxHa4EzrU9Nag&usqp=CAU' />
+        <img className='item' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ2DY2WktwMebkhOn7kGhkwXoxHa4EzrU9Nag&usqp=CAU' />
+        <img className='item' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ2DY2WktwMebkhOn7kGhkwXoxHa4EzrU9Nag&usqp=CAU' />
+        <img className='item' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ2DY2WktwMebkhOn7kGhkwXoxHa4EzrU9Nag&usqp=CAU' />
+        <img className='item' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ2DY2WktwMebkhOn7kGhkwXoxHa4EzrU9Nag&usqp=CAU' />
+      </div>
+    </div>
   );
 }
 
