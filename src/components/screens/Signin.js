0f6cb0d56c4225398e0b5f9745ca8947feb22103
Deepika <bhuvanadeepika017@gmail.com>
2020-09-19 11:23:01 +0530

    added signin and signup components and css for it

--- src/components/screens/Signin.js
+++ src/components/screens/Signin.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Signin = () => {
   return (
-    <h1>Signin</h1>
+    <div className="basic-card">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input
+          type="text"
+          placeholder="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+        />
+        <button className="btn #64b5f6 blue lighten-2" >Signin</button>
+        <h5>
+          <Link to='/signup'>Dont have an account ?</Link>
+        </h5>
+      </div>
+    </div>
   );
 }
 
