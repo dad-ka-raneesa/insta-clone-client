e41077ef8d48659bbe781e15e072b441e026864c
Deepika <bhuvanadeepika017@gmail.com>
2020-09-19 12:36:57 +0530

    added home component and css for it

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div className='home'>
+      <div className='card home-card'>
+        <h5>Isabella</h5>
+        <div className='card-image'>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSf_UzHVgFCy9VJb_yY83CBSXkTvcCkWkzxkg&usqp=CAU' />
+        </div>
+        <div className='card-content'>
+          <i class="material-icons">favorite</i>
+          <h6>title</h6>
+          <p>Beautiful mountains</p>
+          <input type='text' placeholder='add a comment'></input>
+        </div>
+      </div>
+      <div className='card home-card'>
+        <h5>Isabella</h5>
+        <div className='card-image'>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSf_UzHVgFCy9VJb_yY83CBSXkTvcCkWkzxkg&usqp=CAU' />
+        </div>
+        <div className='card-content'>
+          <i class="material-icons">favorite</i>
+          <h6>title</h6>
+          <p>Beautiful mountains</p>
+          <input type='text' placeholder='add a comment'></input>
+        </div>
+      </div>
+      <div className='card home-card'>
+        <h5>Isabella</h5>
+        <div className='card-image'>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSf_UzHVgFCy9VJb_yY83CBSXkTvcCkWkzxkg&usqp=CAU' />
+        </div>
+        <div className='card-content'>
+          <i class="material-icons">favorite</i>
+          <h6>title</h6>
+          <p>Beautiful mountains</p>
+          <input type='text' placeholder='add a comment'></input>
+        </div>
+      </div>
+    </div>
   )
 }
 
